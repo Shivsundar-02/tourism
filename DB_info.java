ff9cbc28dbd1a53933169dfac91e4715af41354c
Shivsundar-02 <shivsundarswain02@gmail.com>
2024-09-14 22:40:14 +0530

    Add files via upload

---
+++ DB_info.java
@@ -0,0 +1,9 @@
+package com.tour;
+
+public interface DB_info {
+	
+	public static final String dburl="jdbc:oracle:thin:@localhost:1521:ORCL";
+	public static final String uname="system";
+	public static final String pwd="tiger";
+
+}
